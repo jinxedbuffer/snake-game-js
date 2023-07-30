f2b8fbf11db20e32fb5a270a73ea5cb199217cc7
Srijan <140931080+sr1j4n@users.noreply.github.com>
2023-07-30 12:25:59 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,5 @@
 function draw() {
   let canvas = document.getElementById("screen");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerWidth;
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
   }
