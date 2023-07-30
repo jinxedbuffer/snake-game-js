e06191902e70e04c5d9f6f10c9002096c4d07fe0
Srijan <140931080+sr1j4n@users.noreply.github.com>
2023-07-30 10:40:29 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
 var h = window.innerHeight;
 var w = window.innerWidth;
-var x = getElementById("screensize");
+var x = document.getElementById("screensize");
 x.innerHTML = "Height: " + h + "Width: " + w;
